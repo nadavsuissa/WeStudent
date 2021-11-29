d09fb231c2fb589593ae46138b8e8a118ba57e6b
NadavSuissa <74867230+nadavsuissa@users.noreply.github.com>
2021-11-29 20:09:40 +0200

    fixed @uria branch +

---
+++ app/src/main/java/com/project/westudentmain/Utils.java
@@ -0,0 +1,9 @@
+package com.project.westudentmain;
+
+//TODO this class contain:
+// - connection to firebase,add\update\remove\withdraw users,
+// - communication with share preferences
+// - sign in with google and facebook (optional)
+
+public class Utils {
+}
