23a5c125158a96466ddc3427075196dc50112f62
NadavSuissa <74867230+nadavsuissa@users.noreply.github.com>
2021-12-09 17:58:56 +0200

    part of gui

--- app/src/main/java/com/project/westudentmain/MainActivity.java
+++ app/src/main/java/com/project/westudentmain/MainActivity.java
@@ -3,20 +3,14 @@ package com.project.westudentmain;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import com.example.androidproject.R;
-
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    public DrawerLayout drawerLayout;
-    public ActionBarDrawerToggle actionBarDrawerToggle;
+
     private Button btn_crt;
+    private Button btn_vpf; // View Profile
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +19,8 @@ public class MainActivity extends AppCompatActivity {
         connect_items_by_id();
 
         btn_crt.setOnClickListener(var -> startActivity(new Intent(MainActivity.this, EditProfileActivity.class)));
+          btn_vpf.setOnClickListener(var -> startActivity(new Intent(MainActivity.this, showProfile.class)));
 
-
-        // drawer layout instance to toggle the menu icon to open
-        // drawer and back button to close drawer
-        drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-
-        // to make the Navigation drawer icon always appear on the action bar
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 
     // override the onOptionsItemSelected()
@@ -50,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void connect_items_by_id() {
         btn_crt = findViewById(R.id.button5);
-
+        btn_vpf = findViewById(R.id.button3);
     }
 
 
