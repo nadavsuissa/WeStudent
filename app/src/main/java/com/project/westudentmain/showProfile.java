ee6f9bdd5ac658d6045c044c34b5ab105fffee91
NadavSuissa <74867230+nadavsuissa@users.noreply.github.com>
2021-12-09 18:04:20 +0200

    part of gui

---
+++ app/src/main/java/com/project/westudentmain/showProfile.java
@@ -0,0 +1,14 @@
+package com.project.westudentmain;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.androidproject.R;
+
+public class showProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_profile);
+    }
+}
