934ba570053b1419075cdf01f23b96cf9b4c3fb3
ishay320 <ishay320@hotmail.com>
2021-12-15 16:44:55 +0200

    profile activity
    xml
    class of profile
    examples for firebase

--- app/src/main/java/com/project/westudentmain/activities/showProfile.java
+++ app/src/main/java/com/project/westudentmain/activities/showProfile.java
@@ -1,11 +1,6 @@
 package com.project.westudentmain.activities;
 
 
-import androidx.annotation.NonNull;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,123 +10,118 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.example.androidproject.R;
-import com.project.westudentmain.classes.Group;
 import com.project.westudentmain.classes.Profile;
 import com.project.westudentmain.classes.User;
 import com.project.westudentmain.util.CustomDataListener;
-import com.project.westudentmain.util.CustomOkListener;
 import com.project.westudentmain.util.FireBaseData;
 
 public class showProfile extends AppCompatActivity {
-    private Toolbar mToolBar;
+    private Toolbar mToolBar; // WTF is that
 
-    private ImageView profile_img;
-    private TextView txt_university, txt_department, txt_degree, txt_year, txt_bio,txt_home_town;
-    private Button btn_settings,appostedbtn,ptilfpartnersbtn,chatbtn;
-//    private User user;
+    private ImageView img_profile; //TODO: show the pic
+    private TextView txt_user_name, txt_name, txt_university, txt_department, txt_degree, txt_year, txt_bio;
+    private Button btn_all_groups, btn_my_groups, btn_friends, btn_users, btn_update_user_data;
+
     private FireBaseData fire_base_data;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_profile);
+        setContentView(R.layout.activity_profile);
         connect_items_by_id();
+
+        //??
         mToolBar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mToolBar);
 
         fire_base_data = FireBaseData.getInstance();
 
-//        add user example:
-//        User user = new User("user_name123", "name", "last_name", FireBaseData.getEmail(), "2134568");
-//        fire_base_data.updateData(user,null);
-
-//        add group example:
-//        Group group = new Group("new group", "String description", 12, "1784");
-//        fire_base_data.addNewGroup(group,null);
-
-
-//        remove group example:
-//        fire_base_data.removeGroup("-Mqu6r2OgiNGU1SBMAgG", new CustomOkListener() {
-//            @Override
-//            public void onComplete(@NonNull String what, Boolean ok) {
-//                Toast.makeText(getBaseContext(), what + ok, Toast.LENGTH_SHORT).show();
-//            }
-//        });
 
         fire_base_data.getUserData(User.class, new CustomDataListener() {
             @Override
             public void onDataChange(@NonNull Object data) {
                 User user = (User) data;
-                //TODO: else ask to add profile and check if working
-                if(user.getProfile() != null)
-                    RenderText(user);
+                RenderText(user);
             }
 
             @Override
             public void onCancelled(@NonNull String error) {
-//                user = new User();
                 Toast.makeText(getBaseContext(), error, Toast.LENGTH_LONG).show();
+                //TODO: jump back to login
             }
         });
 
-//        user deletion example:
-//        FireBaseLogin.deleteUser("user_name", new CustomDataListener() {
-//            @Override
-//            public void onDataChange(@NonNull Object data) {
-//                if ((int)data<0){
-//
-//                    Toast.makeText(getBaseContext(), "user deletion code "+ data, Toast.LENGTH_SHORT).show();
-//                    startActivity(new Intent(showProfile.this,Login.class));
-//                }else
-//                    Toast.makeText(getBaseContext(), "data deletion times "+ data, Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull String error) {
-//                Toast.makeText(getBaseContext(), error, Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
+        //TODO: give them what to look for
+        btn_all_groups.setOnClickListener(view -> {
+            startActivity(new Intent(this, showGroup.class));
+        });
+        btn_my_groups.setOnClickListener(view -> {
+            startActivity(new Intent(this, showGroup.class));
+        });
+        btn_friends.setOnClickListener(view -> {
+            startActivity(new Intent(this, showFriends.class));
+        });
+        btn_users.setOnClickListener(view -> {
+            startActivity(new Intent(this, showFriends.class));
+        });
+        btn_update_user_data.setOnClickListener(view -> {
+            startActivity(new Intent(this, showSettings.class));
+        });
     }
 
-    private void RenderText(User user) {
+    private void RenderText(@NonNull User user) {
+        txt_name.setText(user.getName());
+        txt_user_name.setText(user.getUserName());
+
         Profile profile = user.getProfile();
-        txt_degree.setText("Degree: "+profile.getDegree());
-        txt_department.setText(profile.getDepartment());
-        txt_university.setText(profile.getUniversity());
-        txt_year.setText(""+profile.getYear());
-        txt_bio.setText(profile.getBIO());
-        txt_home_town.setText(profile.getHomeTown());
+        if (profile != null) {
+            txt_university.setText(String.format("University: %s", profile.getUniversity()));
+            txt_department.setText(String.format("Department: %s", profile.getDepartment()));
+            txt_degree.setText(String.format("Degree: %s", profile.getDegree()));
+            txt_year.setText(String.format("Year: %s", profile.getYear()));
+            txt_bio.setText(profile.getBIO());
+        } // TODO: add else
+
     }
 
     private void connect_items_by_id() {
-        profile_img = findViewById(R.id.profilepicview);
-        txt_university = findViewById(R.id.universitytextview);
-        txt_department = findViewById(R.id.departmenttextview);
-        txt_degree = findViewById(R.id.degreetextview);
-        txt_year = findViewById(R.id.yeartextview);
-        txt_bio = findViewById(R.id.biotextview);
-        txt_home_town=findViewById(R.id.fwaitextview); // FWAI - From Where Am I
+        img_profile = findViewById(R.id.profilepicview);
 
-        btn_settings = findViewById(R.id.btn_settings);
-        appostedbtn = findViewById(R.id.btn_apposted);
-        ptilfpartnersbtn = findViewById(R.id.btn_ptilfpartners); // PTILF - Projects that i looked for partners
-        chatbtn = findViewById(R.id.btn_chat);
+        txt_name = findViewById(R.id.textViewName);
+        txt_user_name = findViewById(R.id.textViewUserName);
+        txt_university = findViewById(R.id.textViewUniversity);
+        txt_department = findViewById(R.id.textViewDepartment);
+        txt_degree = findViewById(R.id.textViewDegree);
+        txt_year = findViewById(R.id.textViewYear);
+
+        txt_bio = findViewById(R.id.multiLineTextBio);
+
+        btn_all_groups = findViewById(R.id.buttonAllGroup);
+        btn_my_groups = findViewById(R.id.buttonMyGroup);
+        btn_friends = findViewById(R.id.buttonFriends);
+        btn_users = findViewById(R.id.buttonUsers);
+        btn_update_user_data = findViewById(R.id.buttonUpdateUserData);
 
     }
 
     @Override
     public void onBackPressed() {
-        startActivity(new Intent(showProfile.this,showSettings.class)); // this is how to move between screens
+        startActivity(new Intent(showProfile.this, showSettings.class)); // this is how to move between screens
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
