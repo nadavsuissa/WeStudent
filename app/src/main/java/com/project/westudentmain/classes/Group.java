726e6ad1b3bede034f5e73e0b967038c58a36acf
ishay320 <ishay320@hotmail.com>
2021-12-21 13:36:57 +0200

    in the making:
    upgrade group class
    groups firebase logic

--- app/src/main/java/com/project/westudentmain/classes/Group.java
+++ app/src/main/java/com/project/westudentmain/classes/Group.java
@@ -1,33 +1,43 @@
 package com.project.westudentmain.classes;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Group {
     // the id of the group is in firebase
-    private String group_id=randomKey();
+    private String group_id;
     private String group_name;
     private String description;
     private int max_capacity;
     //zoom/faceToFace:enum
-    private String date;
+    private Object creation_date;
 
-    private ArrayList<User> users;
+    enum user_status {
+        asking,
+        waiting,
+        friend,
+        manager
+    }
+
+    private Map<String, user_status> users;
     private final GroupActivityManager group_activity_manager;
 
-    public Group(String group_name, String description, int max_capacity, String date) {
+    public Group(String group_name, String description, int max_capacity, String creation_date) {
         this.group_name = group_name;
         this.description = description;
         this.max_capacity = max_capacity;
-        this.date = date;
+        this.creation_date = creation_date;
         this.group_activity_manager = new GroupActivityManager();
     }
 
     public Group() {
-        this.group_name = "group_name";
-        this.description = "description";
+
+        this.group_name = "default group name";
+        this.description = "";
         this.max_capacity = 0;
-        this.date = "date";
+        this.creation_date = "";
         this.group_activity_manager = new GroupActivityManager();
     }
 
@@ -55,12 +65,77 @@ public class Group {
         this.description = description;
     }
 
-    public ArrayList<User> getUsers() {
-        return users;
+    public List<String> getAllUsers() {
+        List<String> all_users = new ArrayList<>();
+        this.users.forEach((s, s2) -> {
+            all_users.add(s);
+        });
+
+        return all_users;
     }
 
-    public void setUsers(ArrayList<User> users) {
-        this.users = users;
+    public List<String> getAskingUsers() {
+        List<String> all_users = new ArrayList<>();
+        this.users.forEach((s, s2) -> {
+            if (s2 == user_status.asking) {
+                all_users.add(s);
+            }
+        });
+
+        return all_users;
+    }
+
+    public List<String> getFriendsUsers() {
+        List<String> all_users = new ArrayList<>();
+        this.users.forEach((s, s2) -> {
+            if (s2 == user_status.friend) {
+                all_users.add(s);
+            }
+        });
+
+        return all_users;
+    }
+
+    public List<String> getWaitingUsers() {
+        List<String> all_users = new ArrayList<>();
+        this.users.forEach((s, s2) -> {
+            if (s2 == user_status.waiting) {
+                all_users.add(s);
+            }
+        });
+
+        return all_users;
+    }
+
+    public List<String> getManagerUsers() {
+        List<String> all_users = new ArrayList<>();
+        this.users.forEach((s, s2) -> {
+            if (s2 == user_status.manager) {
+                all_users.add(s);
+            }
+        });
+
+        return all_users;
+    }
+
+    public boolean hasConnection(String friend) {
+        return users.containsKey(friend);
+    }
+
+    public boolean isFriend(String user_name) {
+        return users.get(user_name) == user_status.friend;
+    }
+
+    public boolean isOnAskedList(String user_name) {
+        return users.get(user_name) == user_status.asking;
+    }
+
+    public boolean isOnWaitList(String user_name) {
+        return users.get(user_name) == user_status.waiting;
+    }
+
+    public boolean isOnManagerList(String user_name) {
+        return users.get(user_name) == user_status.manager;
     }
 
     public int getMaxCapacity() {
@@ -72,25 +147,14 @@ public class Group {
     }
 
     public String getDate() {
+        //TODO: check if work
+        String date = creation_date.toString();
+
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-    public String randomKey() {
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
-        Random random = new Random();
-
-        String generatedString = random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-
-        return generatedString;
+    public void setDate(String creation_date) {
+        this.creation_date = creation_date;
     }
 
     //TODO: check if it is a pointer
---
+++ app/src/main/java/com/project/westudentmain/util/FireBaseGroup.java
@@ -0,0 +1,92 @@
+package com.project.westudentmain.util;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+import com.project.westudentmain.classes.Group;
+import com.project.westudentmain.classes.User;
+
+import java.util.Objects;
+
+public class FireBaseGroup {
+    private static final DatabaseReference database_reference = FirebaseDatabase.getInstance().getReference();
+    private final static FireBaseGroup INSTANCE = new FireBaseGroup();
+
+    private FireBaseGroup() {}
+
+    /**
+     * singleton
+     *
+     * @return the FireBaseGroup object
+     */
+    public static FireBaseGroup getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * send and connect Group data to fire base and current user
+     * (fill key,creation date of group and add to current user)
+     *
+     * @param group
+     * @param listener if you want to know about completion pass listener else pass null
+     * @return  if can even do it(user is logged in)
+     */
+    public boolean pushNewGroup(Group group, CustomOkListener listener) { //TODO: in the making
+        FirebaseUser firebaseUser = FireBaseLogin.getUser();
+        if (firebaseUser == null)
+            return false;
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        String key = database.getReference(Group.class.getSimpleName()).push().getKey();
+
+        group.setGroup_id(key);
+        group.setDate(ServerValue.TIMESTAMP);
+
+        database_reference.child(Group.class.getSimpleName()).child(key).setValue(group).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (listener != null) {
+                    if (task.isSuccessful()) {
+                        listener.onComplete("group uploaded successfully", true);
+                        //TODO: add user as manager
+//                        database_reference.child(User.class.getSimpleName()).child(firebaseUser.getUid()).child()
+                    } else
+                        listener.onComplete("group upload failed", false);
+                }
+            }
+        });
+
+        return true;
+    }
+
+    /**
+     * gets global data
+     *
+     * @param object         the object to be getting
+     * @param event_listener the listener for the data or error
+     */
+    public void getGlobalData(@NonNull final Class<?> object, CustomDataListener event_listener) {
+        database_reference.child(object.getSimpleName()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //TODO: need checking
+                event_listener.onDataChange(Objects.requireNonNull(snapshot.getValue(object)));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                event_listener.onCancelled(error.getMessage());
+            }
+        });
+    }
+
+
+}
