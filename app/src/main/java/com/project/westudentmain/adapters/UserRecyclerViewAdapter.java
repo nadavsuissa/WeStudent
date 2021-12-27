f3c9fb40207834797dae939b5e4dedf0205c7fe2
Uriya Shalom <uriyas10@gmail.com>
2021-12-27 14:45:32 +0200

    decline and withdraw buttons addad to users list

--- app/src/main/java/com/project/westudentmain/adapters/UserRecyclerViewAdapter.java
+++ app/src/main/java/com/project/westudentmain/adapters/UserRecyclerViewAdapter.java
@@ -1,31 +1,21 @@
 package com.project.westudentmain.adapters;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.androidproject.R;
-import com.google.android.material.snackbar.Snackbar;
 import com.project.westudentmain.classes.User;
 import com.project.westudentmain.util.CustomDataListener;
-import com.project.westudentmain.util.CustomOkListener;
 import com.project.westudentmain.util.FireBaseData;
-
 import java.util.ArrayList;
 
 public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerViewAdapter.ViewHolder> {
@@ -80,6 +70,21 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerVi
                 holder.button_friend_action.setOnClickListener(view ->{
                     requestAndRemoveFriend(main_user,selected_user,holder);
                 });
+                holder.btn_decline.setBackgroundColor(context.getColor(R.color.red));
+                holder.btn_decline.setOnClickListener(view ->{
+                    FireBaseData.getInstance().removeFriend(selected_user.getUserName(), (what, ok) -> {
+                        if (ok) {
+                            Toast.makeText(context, what, Toast.LENGTH_SHORT).show();
+                            holder.button_friend_action.setText("add");
+                            holder.button_friend_action.setBackgroundColor(context.getColor(R.color.blue));
+                            holder.btn_decline.setVisibility(View.GONE);
+                            holder.button_friend_action.setClickable(true);
+                        } else {
+                            Toast.makeText(context, what, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                });
                 showFriendStatus(main_user,selected_user,holder);
 
             }
@@ -91,15 +96,6 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerVi
         });
 
 
-//        holder.card_root.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                OnPickUserDialog(users.get(position).getUserName(), holder.card_root);
-//                //Toast.makeText(context, users.get(position).getName() + " Selected", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-
     }
 
     private void showFriendStatus(User main_user, User selected_user, ViewHolder holder) {
@@ -109,43 +105,43 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerVi
         }else if (main_user.isFriend(selected_user.getUserName())){
             holder.button_friend_action.setText("remove");
             holder.button_friend_action.setBackgroundColor(context.getColor(R.color.red));
+            holder.btn_decline.setVisibility(View.GONE);
         }else if (main_user.isOnAskedList(selected_user.getUserName())) {
             holder.button_friend_action.setText("waiting");
+            holder.button_friend_action.setClickable(false);
             holder.button_friend_action.setBackgroundColor(context.getColor(R.color.yellow));
+            holder.btn_decline.setVisibility(View.VISIBLE);
+            holder.btn_decline.setText("withdraw");
         }else if (main_user.isOnWaitList(selected_user.getUserName())) {
             holder.button_friend_action.setText("accept");
+            holder.btn_decline.setVisibility(View.VISIBLE);
+            holder.btn_decline.setText("decline");
+
         }
     }
 
     private void requestAndRemoveFriend(User main_user, User selected_user, ViewHolder holder) {
         String btn_status =  holder.button_friend_action.getText().toString();
         if (btn_status.equals("add")) {
+            holder.button_friend_action.setClickable(true);
             FireBaseData.getInstance().askToBeFriend(selected_user.getUserName(), (what, ok) -> {
                 //TODO: ask if it ok in pop up massage
                 if (ok) {
                     Toast.makeText(context, "friend request sent", Toast.LENGTH_SHORT).show();
                     holder.button_friend_action.setText("waiting");
                     holder.button_friend_action.setBackgroundColor(context.getColor(R.color.yellow));
+                    holder.btn_decline.setVisibility(View.VISIBLE);
+                    holder.btn_decline.setText("withdraw");
+                    holder.button_friend_action.setClickable(false);
                 } else {
                     Toast.makeText(context, what, Toast.LENGTH_SHORT).show();
                 }
             });
 
-        }else if (btn_status.equals("remove")){
+        }else if (btn_status.equals("remove")) {
             FireBaseData.getInstance().removeFriend(selected_user.getUserName(), (what, ok) -> {
                 if (ok) {
-                    //Toast.makeText(context, "friend removed", Toast.LENGTH_SHORT).show();
-                    holder.button_friend_action.setText("add");
-                    holder.button_friend_action.setBackgroundColor(context.getColor(R.color.blue));
-                } else {
-                    Toast.makeText(context, what, Toast.LENGTH_SHORT).show();
-                }
-            });
-        }else if (btn_status.equals("waiting")){
-
-            FireBaseData.getInstance().removeFriend(selected_user.getUserName(), (what, ok) -> {
-                if (ok) {
-                    Toast.makeText(context, what, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "friend removed", Toast.LENGTH_SHORT).show();
                     holder.button_friend_action.setText("add");
                     holder.button_friend_action.setBackgroundColor(context.getColor(R.color.blue));
                 } else {
@@ -153,60 +149,18 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerVi
                 }
             });
         }else if (btn_status.equals("accept")){
-            //Todo add decline friend
             FireBaseData.getInstance().acceptFriendRequest(selected_user.getUserName(), (what, ok) -> {
                 if (ok) {
                     Toast.makeText(context, what, Toast.LENGTH_SHORT).show();
                     holder.button_friend_action.setText("remove");
                     holder.button_friend_action.setBackgroundColor(context.getColor(R.color.red));
-
+                    holder.btn_decline.setVisibility(View.GONE);
                 } else {
                     Toast.makeText(context, what, Toast.LENGTH_SHORT).show();
                 }
             });
+
         }
-    }
-    private void OnPickUserDialog(String friend_username, CardView parent) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-
-        builder.setTitle("Choose option:");
-        builder.setItems(new CharSequence[]
-                        {"send friend request", "cancel friend request"},
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        if (which == 0) {
-                            fire_base_data.askToBeFriend(friend_username, (what, ok) -> {
-                                Toast.makeText(context, what, Toast.LENGTH_SHORT).show();
-                            });
-
-                        }
-                        if (which == 1) {
-                            fire_base_data.removeFriend(friend_username, (what, ok) -> {
-                                Toast.makeText(context, what, Toast.LENGTH_SHORT).show();
-                            });
-
-                        }
-                    }
-                });
-        builder.create().show();
-    }
-
-    private void showSnackBar(
-            CardView parent,String friend_username) {
-        String txt_snackbar = "sending request ";
-        Snackbar.make(parent,txt_snackbar+"Undo in 5 seconds", 5000/* can replace with Snackbar.LENGTH_INDEFINITE*/).setAction("Undo", new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // undo request
-                fire_base_data.cancelFriendRequest(friend_username,(what, ok) -> {
-                    Toast.makeText(context, what+ok, Toast.LENGTH_SHORT).show();
-                });
-            }
-        })
-                .setActionTextColor(Color.RED)  // for Action color
-                .setTextColor(Color.WHITE)
-                .show();
     }
 
     @Override
@@ -224,7 +178,7 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerVi
         private CardView card_root;
         private TextView txt_name, txt_username;
         private ImageView image;
-        private Button button_friend_action;
+        private Button button_friend_action,btn_decline;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -232,6 +186,7 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerVi
             txt_name = itemView.findViewById(R.id.Name);
             txt_username = itemView.findViewById(R.id.Username_adapter);
             button_friend_action = itemView.findViewById(R.id.buttonAddAskAcceptRemove);
+            btn_decline = itemView.findViewById(R.id.btn_decline);
             image = itemView.findViewById(R.id.profile);
         }
     }
